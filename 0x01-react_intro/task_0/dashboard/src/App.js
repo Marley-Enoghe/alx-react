7472f4532483e21d7fc8d05c572ba58c495e9b7a
Marley Enogheghase <marleyenogheghase@gmail.com>
2024-04-06 18:56:36 -0700

    task 0

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Holberton_Logo from "./Holberton_Logo.jpg";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={Holberton_Logo} className="App-logo" alt="logo" />
+        <h1>
+         School dashboard
+        </h1>
+      </header>
+      <body className="App-body">
+        <p>
+          Login to access the full dashboard
+        </p>
+      </body>
+      <footer className="App-footer">
+        <p>
+          copyright 2020 - holberton School
+        </p>
+      </footer>
+      </div>
+  );
+}
+
+export default App;
