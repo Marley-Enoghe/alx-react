c2b8ad20afca552bbba7930c31a7174986a3dba4
Marley Enogheghase <marleyenogheghase@gmail.com>
2024-04-10 22:17:06 +0100

    My react

---
+++ 0x01-react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Holberton_Logo from "./Holberton_Logo.jpg";
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <img src={Holberton_Logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <hr />
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor='email'>
+          Email:
+        </label>
+        <input type='email' id='email'/>
+        <label htmlFor='password'> Password:</label>
+        <input type='password' id='password'/>
+        <button>OK</button>
+      </body>
+      <footer className="App-footer">
+        <hr />
+        <p>copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
