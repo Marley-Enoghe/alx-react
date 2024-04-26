9a5e196edf27a9a91b455d4346bad3c4a5f8a0d9
Marley Enogheghase <marleyenogheghase@gmail.com>
2024-04-26 17:55:29 +0100

    done with the tasks

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,16 +1,5 @@
-// modify the function getImmutableObject using Map from Immutable.js
-const {Map} = require ('immutable');
-const  getImmutableObject = (obj) => {
-const immutableMap = Map(obj);
-return immutableMap;
-}
+import { Map } from 'immutable';
 
-const object = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-};
+const getImmutableObject = (object) => Map(object);
 
-const immutableObject = getImmutableObject(object);
-console.log(immutableObject);
+module.exports = getImmutableObject;
